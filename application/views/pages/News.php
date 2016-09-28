2f5930ea4ddbbfc255f766707d326749aefd9b72
deathart <deathart@hotmail.fr>
2016-09-28 20:24:26 +0200

    ADD design commentaire

--- application/views/pages/News.php
+++ application/views/pages/News.php
@@ -10,3 +10,28 @@
         </div>
     </div>
 </div>
+<div id="news">
+    <div class="news_header">Commentaire(s)</div>
+    <div class="news_content">
+        <div class="media text">
+            <div class="media-left">
+                <a href="#">
+                    <img class="media-object" src="https://dummyimage.com/64x64/000/fff" alt="Generic placeholder image">
+                </a>
+            </div>
+            <div class="media-body">
+                <h4 class="media-heading">Media heading</h4>
+                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                <div class="media m-t-2">
+                    <a class="media-left" href="#">
+                        <img class="media-object" src="https://dummyimage.com/64x64/000/fff" alt="Generic placeholder image">
+                    </a>
+                    <div class="media-body">
+                        <h4 class="media-heading">Nested media heading</h4>
+                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
