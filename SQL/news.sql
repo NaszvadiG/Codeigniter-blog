a1bb2ff22e0809654306b19ba6a616accd46b91a
deathart <deathart@hotmail.fr>
2016-10-06 16:12:28 +0200

    FIX

--- SQL/news.sql
+++ SQL/news.sql
@@ -5,7 +5,8 @@ CREATE TABLE IF NOT EXISTS `news` (
   `news` longtext COLLATE utf8_bin NOT NULL,
   `author` int(11) NOT NULL,
   `date_created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `images` varchar(250) COLLATE utf8_bin DEFAULT NULL,
+  `img_home` varchar(250) COLLATE utf8_bin DEFAULT 'https://dummyimage.com/140x140/bfbfbf/000000.png',
+  `img_news` varchar(250) COLLATE utf8_bin DEFAULT 'https://dummyimage.com/140x140/bfbfbf/000000.png',
   `categorie` int(11) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- application/views/errors/html/error_404.php
+++ application/views/errors/html/error_404.php
@@ -1,11 +1,9 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-<div id="news">
-    <div class="news_header">
+<div id="BoxPrimary">
+    <div class="header">
         <?php echo lang('Title_Page_Error_404'); ?>
     </div>
-    <div class="news_content">
-        <div class="text">
-            <?php echo lang('Description_Page_Error_404'); ?>
-        </div>
+    <div class="contentain">
+        <?php echo lang('Description_Page_Error_404'); ?>
     </div>
 </div>
