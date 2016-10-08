462463f1a28fc83ed6c426e143115be879f24164
deathart <deathart@hotmail.fr>
2016-10-08 18:51:09 +0200

    TYPO

--- SQL/categorie.sql
+++ SQL/categorie.sql
@@ -2,7 +2,9 @@ CREATE TABLE IF NOT EXISTS `categorie` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(250) COLLATE utf8_bin NOT NULL DEFAULT '0',
   `description` varchar(250) COLLATE utf8_bin NOT NULL DEFAULT '0',
+  `parents` int(11) DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-INSERT INTO `categorie` (`id`, `name`, `description`) VALUES (1, 'Général', 'Catégorie général');
+INSERT INTO `categorie` (`id`, `name`, `description`, `parents`) VALUES (1, 'Général', 'Catégorie général', 0);
+
